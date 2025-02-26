9936b338cae67c7c0ee153d13056132affed6857
Sierra-Doyle <117295037+Sierra-Doyle@users.noreply.github.com>
2025-02-25 20:46:34 -0500

    added a comments feature to posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from .views import add_comment
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
     path('createPost/', views.createPost, name='createPost'), 
+    path('add_comment/<int:post_id>/', add_comment, name='add_comment')
 ]
 
