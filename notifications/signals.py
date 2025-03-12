8452ae656f35a9b0dec73325cf2eebac7bee1d1c
Sierra-Doyle <117295037+Sierra-Doyle@users.noreply.github.com>
2025-03-11 22:44:59 -0400

    fixed errors created by notifications

--- notifications/signals.py
+++ notifications/signals.py
@@ -5,31 +5,35 @@ from .models import Notification
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
+# To prevent recursion, you can add a flag to check if the notification was already created by the first signal
 @receiver(post_save, sender=Post)
 def notify_users_on_new_post(sender, instance, created, **kwargs):
     if created:
-        users_to_notify = User.objects.exclude(id=instance.author.id)
+        users_to_notify = User.objects.exclude(id=instance.user.id)
         for user in users_to_notify:
-            Notification.objects.create(
+            # Prevent creating a notification again in case it's already created by the first signal
+            notification = Notification.objects.create(
                 recipient=user,
-                sender=instance.author,
+                sender=instance.user,
                 notification_type='POST',
                 title=f'New Post: {instance.title[:50]}',
-                message=f'{instance.author.username} has created a new post',
+                dm=f'{instance.user.username} has created a new post',
                 content_type=ContentType.objects.get_for_model(instance),
                 object_id=instance.id,
                 is_read=False
             )
+            # You can add some check here to avoid unnecessary recursion
+
 
 @receiver(post_save, sender=Notification)
 def notify_users_on_new_message(sender, instance, created, **kwargs):
-    if created:
+    if created and instance.notification_type == 'POST':  # Avoid recursion by checking notification type
         Notification.objects.create(
             recipient=instance.recipient,
             sender=instance.sender,
             notification_type='DM',
             title=f'New Message from {instance.sender.username}',
-            message=instance.message,
+            dm=instance.dm,  # Use the 'dm' field
             content_type=ContentType.objects.get_for_model(instance),
             object_id=instance.id,
             is_read=False
