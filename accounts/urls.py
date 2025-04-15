f23ad8ce267dccb890b019c62800f266ac4e0ac9
Josephmd2k4 <josephmdemarco04@gmail.com>
2025-04-15 16:45:25 -0400

    user profile working!

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts' 
+app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('profile/settings/', views.settings_view, name='settings'),
+    path('profile/', views.redirect_to_own_profile),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
+    path('profile/<str:username>/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/<str:username>/settings/', views.settings_view, name='settings'),
 ]
