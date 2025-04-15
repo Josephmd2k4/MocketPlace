067f7d4b211891036771a602282d99e35be7dd78
Josephmd2k4 <josephmdemarco04@gmail.com>
2025-04-15 17:53:36 -0400

    added other peoples pages, added links to their pages, added different buttons on their pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import PasswordChangeForm
-from django import forms
 from .models import Profile
 
 class ProfileForm(forms.ModelForm):
@@ -18,11 +16,15 @@ class ProfileForm(forms.ModelForm):
         required=True
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-        required=True
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'readonly': 'readonly',  # Make email read-only
+            'style': 'background-color:#f1f1f1;'  # Optional: gray it out
+        }),
+        required=False  # Not required for submission anymore
     )
     profile_image = forms.ImageField(
-        required=False, 
+        required=False,
         widget=forms.FileInput(attrs={'class': 'form-control'})
     )
 
@@ -42,30 +44,49 @@ class ProfileForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)  # Get the user object
+        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
         if user:
             self.fields['first_name'].initial = user.first_name
             self.fields['last_name'].initial = user.last_name
             self.fields['email'].initial = user.email
 
+    def clean_email(self):
+        # Always return initial email; don't allow changes
+        return self.initial.get('email')
+
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text="Required.")
     last_name = forms.CharField(max_length=30, required=True, help_text="Required.")
+    email = forms.EmailField(required=True, help_text="Must be an flsouthern.edu email.")
 
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.lower().endswith('@flsouthern.edu'):
+            raise forms.ValidationError("Email must be a flsouthern.edu address.")
+        return email
+
 User = get_user_model()
 
 class CustomUserChangeForm(forms.ModelForm):
+    email = forms.EmailField(
+        required=False,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'readonly': 'readonly',
+            'style': 'background-color:#f1f1f1;'
+        })
+    )
     profile_image = forms.ImageField(
-        required=False, 
+        required=False,
         widget=forms.FileInput(attrs={'class': 'form-control'})
     )
     bio = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Tell us about yourself...'}), 
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Tell us about yourself...'}),
         required=False
     )
     year_in_school = forms.ChoiceField(
@@ -75,12 +96,12 @@ class CustomUserChangeForm(forms.ModelForm):
             ('Junior', 'Junior'),
             ('Senior', 'Senior'),
             ('Graduate', 'Graduate')
-        ], 
-        widget=forms.Select(attrs={'class': 'form-control'}), 
+        ],
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=False
     )
     major = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your major...'}), 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your major...'}),
         required=False
     )
 
@@ -93,11 +114,13 @@ class CustomUserChangeForm(forms.ModelForm):
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
 
+    def clean_email(self):
+        return self.initial.get('email')
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['new_password2'].label = 'Confirm New Password'  # Customize the label
-        self.fields['new_password2'].help_text = ''  # Remove the help text
+        self.fields['new_password2'].label = 'Confirm New Password'
+        self.fields['new_password2'].help_text = ''
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
